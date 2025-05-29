7d725eb5110fc522fd45d2f41b0a9e1357f33102
YouHaoYang <462153285@qq.com>
2025-05-30 00:50:05 +0800

    实现了洪水卡的正确抽取  能够按照先抽取24张  等不够抽取后重新打乱抽取弃牌堆中的卡片

--- src/Model/Deck/FloodDeck.java
+++ src/Model/Deck/FloodDeck.java
@@ -16,6 +16,7 @@ public class FloodDeck extends Deck<FloodCard> {
     /**
      * 构造函数
      * 根据初始瓦片列表创建洪水卡牌堆
+     * 
      * @param initialTiles 初始瓦片列表，每个瓦片对应一张洪水卡
      */
     public FloodDeck(List<Tile> initialTiles) {
@@ -28,6 +29,7 @@ public class FloodDeck extends Deck<FloodCard> {
 
     /**
      * 获取所有被淹没的瓦片
+     * 
      * @return 当前状态为FLOODED的瓦片列表
      */
     public List<Tile> getFloodedTiles() {
@@ -39,6 +41,7 @@ public class FloodDeck extends Deck<FloodCard> {
 
     /**
      * 获取所有未被淹没的瓦片
+     * 
      * @return 当前在抽牌堆中的洪水卡对应的瓦片列表
      */
     public List<Tile> getUnfloodedTiles() {
@@ -49,6 +52,7 @@ public class FloodDeck extends Deck<FloodCard> {
 
     /**
      * 检查指定瓦片是否被淹没
+     * 
      * @param tile 要检查的瓦片
      * @return 如果瓦片对应的洪水卡在弃牌堆中则返回true
      */
@@ -59,6 +63,7 @@ public class FloodDeck extends Deck<FloodCard> {
 
     /**
      * 获取指定瓦片对应的洪水卡
+     * 
      * @param tile 目标瓦片
      * @return 对应的洪水卡，如果不存在则返回null
      */
@@ -71,6 +76,7 @@ public class FloodDeck extends Deck<FloodCard> {
 
     /**
      * 检查是否所有瓦片都被淹没
+     * 
      * @return 如果抽牌堆为空且弃牌堆不为空则返回true
      */
     public boolean areAllTilesFlooded() {
@@ -89,6 +95,7 @@ public class FloodDeck extends Deck<FloodCard> {
     /**
      * 获取可抽取的洪水卡
      * 只返回与未被沉没的地块对应的卡牌
+     * 
      * @return 可抽取的洪水卡列表
      */
     private List<FloodCard> getDrawableCards() {
@@ -100,18 +107,19 @@ public class FloodDeck extends Deck<FloodCard> {
     /**
      * 抽牌
      * 重写父类方法，每次抽牌前都进行洗牌
+     * 
      * @return 抽到的卡牌，如果没有可抽取的卡牌则返回null
      */
     @Override
     public FloodCard draw() {
-        // 每次抽牌前都进行洗牌
-        reshuffleDiscardPile();
-
+        // 只有在抽牌堆为空时才reshuffle弃牌堆
+        if (drawPile.isEmpty()) {
+            reshuffleDiscardPile();
+        }
         List<FloodCard> drawableCards = getDrawableCards();
         if (drawableCards.isEmpty()) {
             return null;
         }
-
         // 从可抽取的卡牌中随机选择一张
         int randomIndex = (int) (Math.random() * drawableCards.size());
         FloodCard selectedCard = drawableCards.get(randomIndex);
